7efd7d5fbcf67e8a7cbb3a3ff0f4d8fb47d490ce
Max van Essen <max.vanessen@stimmt.digital>
2022-08-26 12:00:54 +0200

    Initial Commit

---
+++ src/test/kotlin/com/maxvanessen/workshop/WorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.maxvanessen.workshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
